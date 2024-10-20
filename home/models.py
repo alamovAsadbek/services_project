37a0b6544fa5a998a02aa96bb3cd296f342cfc98
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-21 01:20:17 +0500

    change models.py

--- home/models.py
+++ home/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+from common.models import BaseModel
 
-class BaseCarouselModel(models.Model):
+
+class BaseCarouselModel(BaseModel):
     title = models.CharField(max_length=255)
     description = models.TextField()
     image = models.ImageField(upload_to='carousel')
