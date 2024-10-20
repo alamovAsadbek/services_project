a1ba8c4a2a1e16f84faf5359bb0c3a61dfb4b383
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-21 02:02:13 +0500

    change

--- social_network/models.py
+++ social_network/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
+from common.models import BaseModel
 
-class SocialNetworkModel(models.Model):
+
+class SocialNetworkModel(BaseModel):
     facebook = models.CharField(max_length=200, null=True, blank=True)
     instagram = models.CharField(max_length=200, null=True, blank=True)
     youtube = models.CharField(max_length=200, null=True, blank=True)
@@ -12,3 +14,14 @@ class SocialNetworkModel(models.Model):
     class Meta:
         verbose_name_plural = "Ijtimoiy tarmoqlar"
         verbose_name = "Ijtimoiy tarmoq"
+
+
+class SiteNameModel(BaseModel):
+    site_name = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return self.site_name
+
+    class Meta:
+        verbose_name_plural = "Sayt nomlari"
+        verbose_name = "Sayt nomi"
