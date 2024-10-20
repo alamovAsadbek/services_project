09b9fc0af0ca9f56702b2639568c824d5e97e52f
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-21 02:14:07 +0500

    change

--- about_us/models.py
+++ about_us/models.py
@@ -4,10 +4,10 @@ from common.models import BaseModel
 
 
 class AboutUsModel(BaseModel):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image1 = models.ImageField(upload_to='about_us')
-    image2 = models.ImageField(upload_to='about_us')
+    title = models.CharField(max_length=200, verbose_name='Sarlavha')
+    description = models.TextField(verbose_name='Matn')
+    image1 = models.ImageField(upload_to='about_us', verbose_name='Rasm1')
+    image2 = models.ImageField(upload_to='about_us', verbose_name='Rasm2')
 
     def __str__(self):
         return self.title
