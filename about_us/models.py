7f35017e73685ed7c4eebf43c371ee2e7c6bf305
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-21 01:36:40 +0500

    change

--- about_us/models.py
+++ about_us/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from common.models import BaseModel
+
+
+class AboutUs(BaseModel):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image1 = models.ImageField(upload_to='about_us')
+    image2 = models.ImageField(upload_to='about_us')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Kompaniyaning Haqida"
+        verbose_name = "Kompaniya Haqida"
--- home/models.py
+++ home/models.py
@@ -6,4 +6,4 @@ from common.models import BaseModel
 class BaseCarouselModel(BaseModel):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    image = models.ImageField(upload_to='carousel')
+    background_image = models.ImageField(upload_to='carousel')
