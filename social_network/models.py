f1c55b686c6900fa28f9f1ddb85fc7c5914a8587
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-21 01:05:10 +0500

    change models.py

--- social_network/models.py
+++ social_network/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class SocialNetworkModel(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.URLField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Social Networks"
+        verbose_name = "Social Network"
