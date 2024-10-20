89d6d2f9cc2cb6f4bf38b1958f8634823a54dc98
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-21 02:08:21 +0500

    chnag

--- home/models.py
+++ home/models.py
@@ -4,9 +4,9 @@ from common.models import BaseModel
 
 
 class BaseCarouselModel(BaseModel):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    background_image = models.ImageField(upload_to='carousel')
+    title = models.CharField(max_length=255, verbose_name='Sarlavha')
+    description = models.TextField(verbose_name='Matn')
+    background_image = models.ImageField(upload_to='carousel', verbose_name='Asosiy rasm')
 
     def __str__(self):
         return self.title
