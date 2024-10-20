276f483300f7e8a23803c782f9db18a6e635c52c
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-21 01:50:10 +0500

    change models.py

--- services/models.py
+++ services/models.py
@@ -16,3 +16,17 @@ class ServiceModel(BaseModel):
     class Meta:
         verbose_name_plural = "Xizmatlarimiz"
         verbose_name = "Xizmet"
+
+
+class FeatureModel(BaseModel):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='features')
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Xususiyatlari"
+        verbose_name = "Xususiyat"
