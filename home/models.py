e65296883da1396b37fbb47872747a3674df4ef6
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-21 02:05:16 +0500

    change

--- home/models.py
+++ home/models.py
@@ -7,3 +7,10 @@ class BaseCarouselModel(BaseModel):
     title = models.CharField(max_length=255)
     description = models.TextField()
     background_image = models.ImageField(upload_to='carousel')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Asosiy Karusel'
+        verbose_name_plural = 'Asosiy Karusellar'
