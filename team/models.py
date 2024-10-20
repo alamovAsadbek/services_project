233dc59211e05ac69d62e6a6ee4e3b845177130b
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-21 01:55:15 +0500

    change models.py

--- team/models.py
+++ team/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from common.models import BaseModel
+
+
+class TeamModel(BaseModel):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    profession = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='team', blank=True, null=True)
+    facebook_url = models.URLField(blank=True, null=True)
+    instagram_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        verbose_name = 'Xodim'
+        verbose_name_plural = 'Xodimlar'
