c37be0ea527654de79bfdf4d8df4cba1e484861b
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-21 01:46:03 +0500

    change models.py

--- services/models.py
+++ services/models.py
@@ -6,9 +6,9 @@ from common.models import BaseModel
 class ServiceModel(BaseModel):
     title = models.CharField(max_length=255)
     description = models.TextField()
+    image = models.ImageField(upload_to='services')
     price = models.DecimalField(max_digits=10, decimal_places=2)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
-
