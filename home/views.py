3711d130de6be02b1a570e65fc2bbb5850558482
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-21 11:23:00 +0500

    change views.py

--- home/views.py
+++ home/views.py
@@ -7,9 +7,6 @@ class HomeView(View):
         return render(request, 'index.html')
 
 
-class AboutView(View):
-    def get(self, request):
-        return render(request, 'about.html')
 
 
 class ServicesView(View):
