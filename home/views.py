f460cadca21e998f5496c9a1004c2568fb998d4d
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-21 11:21:24 +0500

    add ServicesView class in views.py

--- home/views.py
+++ home/views.py
@@ -10,3 +10,8 @@ class HomeView(View):
 class AboutView(View):
     def get(self, request):
         return render(request, 'about.html')
+
+
+class ServicesView(View):
+    def get(self, request):
+        return render(request, 'service.html')
