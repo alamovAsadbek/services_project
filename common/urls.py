6e7fde88bcc275bfd857b09a3ea51a67920260e0
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-21 11:24:06 +0500

    change

--- common/urls.py
+++ common/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from about_us.views import *
 from home.views import *
+from services.views import *
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
