832ffb53833156a316b07298333c6481ca91b2a2
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-22 12:05:43 +0500

    change

--- home/views.py
+++ home/views.py
@@ -7,7 +7,7 @@ from social_network.models import SiteModel
 class HomeView(View):
     def get(self, request):
         site_information = SiteModel.objects.all()
-        print(site_information[0].site_name)
+        print(site_information[0])
         context = {
             'site_information': site_information,
         }
