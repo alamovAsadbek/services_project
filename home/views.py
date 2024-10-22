1532bbe1e34e1b0ea0fcadaf5875a73ca286234e
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-22 12:26:34 +0500

    change

--- home/views.py
+++ home/views.py
@@ -4,22 +4,20 @@ from django.views import View
 from social_network.models import SiteModel
 
 
-class HomeView(View):
-    def get(self, request):
-        site_information = SiteModel.objects.all()
-        print(site_information[0])
-        context = {
-            'site_information': site_information,
-        }
-        BaseView.as_view()
-        return render(request, 'index.html', context)
-
-
 class BaseView(View):
     def get(self, request):
         site_information = SiteModel.objects.all()
-        print(site_information[0])
         context = {
             'site_information': site_information,
         }
+        print(context)
         return render(request, 'layouts/base/base.html', context)
+
+
+class HomeView(BaseView):
+    def get(self, request):
+        site_information = SiteModel.objects.all()
+        context = {
+            'site_information': site_information,
+        }
+        return render(request, 'index.html', context)
