45ff793a0507ab00f3cd1b047ff2d46df89d4653
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-22 15:56:24 +0500

    change

--- home/views.py
+++ home/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 
+from home.models import BaseCarouselModel
 from social_network.models import SiteModel
 
 
 class BaseView(View):
     def get(self, request):
         site_information = SiteModel.objects.all()
+        carousel_information = BaseCarouselModel.objects.all()
         context = {
             'site_information': site_information,
+            'carousel_information': carousel_information
         }
         print(context)
         return render(request, 'layouts/base/base.html', context)
