73b537100bce5d51e2c46e9fba253d85305e583d
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-22 12:14:04 +0500

    change views.py

--- home/views.py
+++ home/views.py
@@ -11,5 +11,15 @@ class HomeView(View):
         context = {
             'site_information': site_information,
         }
-
+        BaseView.as_view()
         return render(request, 'index.html', context)
+
+
+class BaseView(View):
+    def get(self, request):
+        site_information = SiteModel.objects.all()
+        print(site_information[0])
+        context = {
+            'site_information': site_information,
+        }
+        return render(request, 'layouts/base/base.html', context)
