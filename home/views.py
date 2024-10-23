14db74337deb1f89ca63fc1431af56db92bacb03
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-23 23:29:57 +0500

    change views.py

--- home/views.py
+++ home/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import View
 
 from about_us.models import AboutUsModel
-from services.models import ServiceModel
+from services.models import ServiceModel, FeatureModel
 from social_network.models import SiteModel
 from .models import BaseCarouselModel
 
@@ -26,6 +26,8 @@ class HomeView(BaseView):
         context = self.get_context_data()
         about = AboutUsModel.objects.all()
         service = ServiceModel.objects.filter(is_active=True)
+        featured_service = FeatureModel.objects.filter(is_featured=True)
         context['services'] = service
         context['about'] = about
+        context['featured_service'] = featured_service
         return render(request, 'index.html', context)
