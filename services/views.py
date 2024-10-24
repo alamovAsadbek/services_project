eb5fff09d5d7e75f0822494961c3e779274f018d
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-24 10:42:06 +0500

    change views.pt

--- services/views.py
+++ services/views.py
@@ -2,11 +2,19 @@ from django.shortcuts import render
 from django.views import View
 
 from home.views import BaseView
+from services.models import ServiceModel
+from team.models import ReviewModel
 
 
 class ServicesView(BaseView):
     def get(self, request):
         context = self.get_context_data()
+
+        service = ServiceModel.objects.filter(is_active=True)
+        review = ReviewModel.objects.all()
+
+        context['services'] = service
+        context['reviews'] = review
         return render(request, 'service.html', context)
 
 
