e51766947cc3838e603437b8b21be9a7459cd0e1
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-24 11:32:54 +0500

    change views.py

--- team/views.py
+++ team/views.py
@@ -5,5 +5,5 @@ from home.views import BaseView
 
 class ContactView(BaseView):
     def get(self, request):
-        contex = self.get_context_data()
-        return render(request, 'contact.html', contex)
+        context = self.get_context_data()
+        return render(request, 'contact.html', context)
