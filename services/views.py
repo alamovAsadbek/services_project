3729952d4879f0d945fed86609adf7ec9174330f
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-24 10:41:04 +0500

    change views.py

--- services/views.py
+++ services/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.views import View
 
+from home.views import BaseView
 
-class ServicesView(View):
+
+class ServicesView(BaseView):
     def get(self, request):
-        return render(request, 'service.html')
+        context = self.get_context_data()
+        return render(request, 'service.html', context)
 
 
 class WorksView(View):
