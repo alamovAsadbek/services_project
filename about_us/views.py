5d01642b6c4b14f811fb4e979d4e4f223efbbab1
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-24 10:38:35 +0500

    change views.py

--- about_us/views.py
+++ about_us/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-from django.views import View
 
 from home.views import BaseView
+from services.models import FeatureModel
+from team.models import TeamModel
 from .models import AboutUsModel
 
 
 class AboutView(BaseView):
     def get(self, request):
-        super().get(request)
+        context = self.get_context_data()
+
         about = AboutUsModel.objects.all()
-        context = {
-            'about': about
-        }
+        featured_service = FeatureModel.objects.filter(is_active=True)
+        team = TeamModel.objects.all()
+
+        context['about'] = about
+        context['featured_service'] = featured_service
+        context['team'] = team
+        
         return render(request, 'about.html', context)
