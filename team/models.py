43d1f854ce8e1ed1fe46782f058793636f36cb77
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-24 10:05:14 +0500

    change

--- team/models.py
+++ team/models.py
@@ -1,13 +1,32 @@
+from PIL import Image as PilImage
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from common.models import BaseModel
+
+
+# validation
+def validate_image_format(image):
+    try:
+        img = PilImage.open(image)
+        if img.format not in ['JPEG', 'JPG', 'PNG']:
+            raise ValidationError('Image must be in JPEG or PNG format.')
+    except Exception:
+        raise ValidationError('Invalid image file.')
+
+
+def validate_image_dimensions(image):
+    img = PilImage.open(image)
+    if img.size != (300, 300):
+        raise ValidationError('Image dimensions must be exactly 300x300 pixels.')
 
 
 class TeamModel(BaseModel):
     first_name = models.CharField(max_length=80, verbose_name='Ism')
     last_name = models.CharField(max_length=80, verbose_name='Familiya')
     profession = models.CharField(max_length=100, verbose_name='Mutaxassislik')
-    image = models.ImageField(upload_to='team', blank=True, null=True, verbose_name='Rasm')
+    image = models.ImageField(upload_to='team', blank=True, null=True,
+                              validators=[validate_image_dimensions, validate_image_format], verbose_name='Rasm')
     facebook_url = models.URLField(blank=True, null=True, verbose_name='Facebook URL',
                                    help_text="Bu yerda Facebook akkauntini link kurinishda berish kerak")
     instagram_url = models.URLField(blank=True, null=True, verbose_name='Instagram URL',
