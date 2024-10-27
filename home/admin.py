e311e712baf2e92fd9a4dcf58bcaff7a055385ea
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-27 11:44:03 +0500

    change admin.py

--- home/admin.py
+++ home/admin.py
@@ -4,5 +4,5 @@ from .models import *
 
 admin.site.site_header = 'EcoGaz Admin'
 admin.site.site_title = 'EcoGaz Admin Portal'
-admin.site.index_title = 'Welcome to EcoGaz Admin Portal'
+admin.site.index_title = 'Xush Kelibsiz EcoGaz Admin Portal ga'
 admin.site.register(BaseCarouselModel)
