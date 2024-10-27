f10723568c87f730e18c527b2effe256fa47ce9b
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-27 21:07:46 +0500

    change settings.py

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', 'http://104.248.158.101/', '127.0.0.1']
 
 # Application definition
 
