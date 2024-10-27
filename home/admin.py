eda76319363a7edbbb499d3155cca9bc3146939f
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-10-27 11:37:24 +0500

    change admin.py

--- home/admin.py
+++ home/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 
 from .models import *
 
+admin.site.site_header = 'EcoGaz Admin'
+admin.site.site_title = 'EcoGaz Admin Portal'
+admin.site.index_title = 'Welcome to EcoGaz Admin Portal'
 admin.site.register(BaseCarouselModel)
