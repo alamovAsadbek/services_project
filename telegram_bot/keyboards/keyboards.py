0a6350d04df44da2e3a800baec97626abf3bfffe
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-11-21 16:03:15 +0500

    change

--- telegram_bot/keyboards/keyboards.py
+++
@@ -1,6 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-main_keyboards = ReplyKeyboardMarkup(resize_keyboard=True)
-
-main_keyboards.add(KeyboardButton('Xabar Yuborish ✍️'))
-main_keyboards.add(KeyboardButton("Bog'lanish ☎️"))
